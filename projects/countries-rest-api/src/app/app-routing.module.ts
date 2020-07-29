2f75589de8b19be2003388edf8534673d75f263c
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-07-29 23:47:01 +0200

    app-routing routes changed and header added to appComponent

--- projects/countries-rest-api/src/app/app-routing.module.ts
+++ projects/countries-rest-api/src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
   { path: 'detail', loadChildren: () => import('./features/country-detail/country-detail.module').then(m => m.CountryDetailModule)},
-  { path: '**', redirectTo: 'home' }
+  // { path: '**', redirectTo: 'home' }
 ];
 
 @NgModule({
