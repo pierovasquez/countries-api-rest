c79e7d9b9ffc7fb8a8a7747754905a5878e434d1
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-08-01 17:41:02 +0200

    font-awesome added to the project and stlyes reseted to 0

--- projects/countries-rest-api/src/app/app.module.ts
+++ projects/countries-rest-api/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './features/home/home.module';
-import { SharedModule } from './shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     CoreModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/countries-rest-api/src/app/features/home/home.module.ts
+++ projects/countries-rest-api/src/app/features/home/home.module.ts
@@ -4,15 +4,21 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared/shared.module';
+import { FilterInputComponent } from './filter-input/filter-input.component';
+import { FilterSelectComponent } from './filter-select/filter-select.component';
+import { HomeService } from './home.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, FilterInputComponent, FilterSelectComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    SharedModule
+    SharedModule,
+    FontAwesomeModule
   ],
-  exports: [HomeComponent]
+  exports: [HomeComponent],
+  providers: [HomeService]
 })
 export class HomeModule { }
