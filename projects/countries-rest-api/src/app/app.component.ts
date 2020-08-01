d6519c5b49484aac07cf2305bde4e75fb5ec70ed
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-08-01 17:41:32 +0200

    dark/light theme toggler added to the project

--- projects/countries-rest-api/src/app/app.component.ts
+++ projects/countries-rest-api/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'piero-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'countries-rest-api';
+export class AppComponent implements OnInit {
+
+  faMoon = faMoon;
+  storedTheme: string | null = localStorage.getItem('theme-color') || 'theme-light';
+
+  constructor() { }
+  ngOnInit() { }
+
+  toogleThemeMode() {
+    if (this.storedTheme === 'theme-dark') {
+      localStorage.setItem('theme-color', 'theme-light');
+      this.storedTheme = localStorage.getItem('theme-color');
+    } else {
+      localStorage.setItem('theme-color', 'theme-dark');
+      this.storedTheme = localStorage.getItem('theme-color');
+    }
+  }
 }
